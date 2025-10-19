5490a20c34239b0b3bbc14afd1a28b79720d0c31
yavarhadi <yvr.hadi@gmail.com.com>
2025-10-19 21:18:51 +0200

    correction in tt_um_rc_servo_xy_ea.v  module name tt_um_rc_servo_xy

--- src/tt_um_rc_servo_motor_xy_ea.v
+++ src/tt_um_rc_servo_motor_xy_ea.v
@@ -676,7 +676,7 @@ module delta_adc_xy
     .data_o(\moving_average_y_port.data_o ));
 endmodule
 
-module rc_servo_xy
+module tt_um_rc_servo_xy
   (input  clk_i,
    input  reset_i,
    input  comp_async_x_i,
