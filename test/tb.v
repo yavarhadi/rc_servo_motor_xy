f65e5dc71d7e43cec13c98dc289a696459050b9f
yavarhadi <yvr.hadi@gmail.com.com>
2025-10-19 22:17:44 +0200

    tb.v module name correction

--- test/tb.v
+++ test/tb.v
@@ -4,7 +4,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module tb ;
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
