cd7a83d4755f6ce1815049e8d55c08a9323d1329
yavarhadi <yvr.hadi@gmail.com.com>
2025-10-20 18:56:39 +0200

    tb.v syntext modified_01

--- test/tb.v
+++ test/tb.v
@@ -8,10 +8,10 @@ that can be driven / tested by the cocotb test.py
 */
 
 // testbench is controlled by test.py
-module tb ();
+ module tb ();
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
-    initial begin
+   initial begin
         $dumpfile ("tb.vcd");
         $dumpvars (0, tb);
         #1;
@@ -33,20 +33,20 @@ module tb ();
     wire VGND = 1'b0;
   `endif
 
- tt_um_rc_servo_motor_xy_ea (
+    tt_um_rc_servo_motor_xy_ea (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR(VPWR),
         .VGND(VGND),
     `endif
-       .ui_in      (ui_in),    // Dedicated inputs
-       .uo_out     (uo_out),   // Dedicated outputs
-       .uio_in     (uio_in),   // IOs: Input path
-       .uio_out    (uio_out),  // IOs: Output path
-       .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-       .ena        (ena),      // enable - goes high when design is selected
-       .clk        (clk),      // clock
-       .rst_n      (rst_n)     // not reset
-   );
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
 
-endmodule
+ endmodule
